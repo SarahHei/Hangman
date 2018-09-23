176c9a7a0731ec2b28efa8ff0c1ea15ff06c7187
SarahHei <sarahhei@compasseval.com>
2018-09-23 13:42:12 -0400

    Homework Revision 4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,86 +1,85 @@
-<!DOCTYPE html>
-<html lang="en-us">
+//start by creating a word bank array of words around the theme of hurricanes
+var wordbank = [
+    "flood",
+    "storm",
+    "beer",
+    "hurricane",
+    "florence"
+];
 
-<head>Welcome to Hangman!
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-</head>
+//declare all needed variables first
+//introduce two elements into html, to create a space on the page to place the scores; the html will contain a reference to the elements and then look to the javascript to grab the element; there need to be references to the elements in the html
+//this information captures how html and javascript talk to each other; this is not the logic of the game yet
+var correctChoices = 0;
+var incorrectChoices = 0;
+var correctChoicesText = document.getElementById("CorrectChoices");
+var incorrectChoicesText = document.getElementById("IncorrectChoices");
+correctChoicesText.textContent = correctChoices;
+incorrectChoicesText.textContent = incorrectChoices;
 
-<body>
-
-<script type="text/javascript">
-    var wordbank= [
-        "fran", 
-        "storm", 
-        "beer", 
-        "hurricane", 
-        "florence"
-        ];
-
-    var word = wordbank[Math.floor(Math.random() * wordbank.length)];
-
-    var answerArray = [];
-    for (var i = 0; i < word.length; i++) {
-        answerArray[i] = "_";
-    }
-    var remainingLetters = word.length;
-
-    var start = prompt("Press any key to get started");
-    console.log(start);
-  
-
-    //step 2b: for each letter that the user chooses, it is either displayed
-    //in the blank space or in a NOPE location
-    while (remainingLetters > 0); {
-        alert(answerArray.join(" "));
-        var userGuess = prompt("Pick a letter, any letter");
-        if (guess === null) {
-            break;
-        } else if (guess.length !== 1) {
-            alert("Enter only one letter!")
-        } else {
-            for (var j = 0; j < word.length; j++) {
-                if (word[j] === userGuess) {
-                    answerArray[j] = userGuess;
-                    remainingLetters--;
-                }
-            }
-        }
-    }
-        
-            
-            
-
-    //step 3: the user gets to choose the number of letters that matches
-    //word length +1; the computer tells the user how many attempts are remaining
-
-
-    //step 4: if the player wins, the computer says "You Win" and click any key
-    //to start again; if the player loses, the computer says "Sorry, Try Again" and
-    //click any key to try again
-
-
-   /*    
-       var remainingLetters = word.length;
-    while (remainingLetters > 0) {
-    
-        var guess = prompt("Pick a letter, any letter");
-        if (guess === null) {
-            break;
-        } else if (guess.length !== 1) {
-            alert("Enter one letter only");
-        } else {
-            for (var j = 0; j < word.length; j++) {
-                if (word[j] === guess) {
-                    answerArray[j] = guess;
-                    remainingLetters--;
-                }
-            }
-        }
-
-    }
+/*var attempts = 0;
+var numberAttempts = document.getElementById("NumberofAttempts");
+attempts = 10;
 */
 
-</script>
-</body>
-</html>
+var answerArrayDiv = document.getElementById("AnswerArrayDiv");
+
+var splitWord = [];
+var userGuess;
+var answerArray = [];
+
+//from this point on, we are playing the game
+//when we tell the computer to do something, we have to introduce a function
+//declare the syntax for the function
+//execute the syntax
+//step 1a: the computer chooses a random word from the word bank
+
+/*function attemptsFunction() {
+    console.log("This works");
+    numberAttempts.textContent = attempts;
+}
+attemptsFunction();
+*/
+
+function startGame() {
+    var word = wordbank[Math.floor(Math.random() * wordbank.length)];
+    console.log(word);
+    splitWord = word.split("");
+    console.log(splitWord);
+
+    //assign underscores based on word length for randomly chosen word
+    var answerArray = [];
+    for (var i = 0; i < splitWord.length; i++) {
+        answerArray[i] = "_";
+    }
+
+    console.log(answerArray);
+    answerArrayDiv.textContent = answerArray;
+}
+startGame();
+
+//match user's guess to the answer array and assign to the right spot
+//when there is a match, the answerArray needs to display the match at that position; change from underscore to the letter
+//if splitword at j = userGuess at position XX, then change the answerArray at j
+
+function playGame(x) {
+    for (var j = 0; j < splitWord.length; j++) {
+        if (splitWord[j] === x) {
+        answerArray[j] = x;
+        console.log("Match");
+            }
+        }
+    }
+
+document.onkeyup = function (event) {
+    var userGuess = event.key;
+    console.log(userGuess);
+    playGame(userGuess);
+}
+
+//check out innerHTML versus textContent
+
+
+
+
+
