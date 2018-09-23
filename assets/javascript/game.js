4c924cdfeb4005758834640bc56be1c84238ba40
SarahHei <sarahhei@compasseval.com>
2018-09-23 14:50:59 -0400

    Homework Revision 5

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,12 +10,13 @@ var wordbank = [
 //declare all needed variables first
 //introduce two elements into html, to create a space on the page to place the scores; the html will contain a reference to the elements and then look to the javascript to grab the element; there need to be references to the elements in the html
 //this information captures how html and javascript talk to each other; this is not the logic of the game yet
-var correctChoices = 0;
-var incorrectChoices = 0;
-var correctChoicesText = document.getElementById("CorrectChoices");
+
+var correctGuess = false;
+var remainingGuesses = 10;
+var incorrectChoices = [];
 var incorrectChoicesText = document.getElementById("IncorrectChoices");
-correctChoicesText.textContent = correctChoices;
 incorrectChoicesText.textContent = incorrectChoices;
+
 
 /*var attempts = 0;
 var numberAttempts = document.getElementById("NumberofAttempts");
@@ -48,27 +49,40 @@ function startGame() {
     console.log(splitWord);
 
     //assign underscores based on word length for randomly chosen word
-    var answerArray = [];
     for (var i = 0; i < splitWord.length; i++) {
         answerArray[i] = "_";
     }
 
-    console.log(answerArray);
     answerArrayDiv.textContent = answerArray;
 }
 startGame();
+    console.log(answerArray);
 
 //match user's guess to the answer array and assign to the right spot
 //when there is a match, the answerArray needs to display the match at that position; change from underscore to the letter
 //if splitword at j = userGuess at position XX, then change the answerArray at j
+//once a variable is defined globally, don't use var again
 
 function playGame(x) {
     for (var j = 0; j < splitWord.length; j++) {
         if (splitWord[j] === x) {
-        answerArray[j] = x;
-        console.log("Match");
+        correctGuess = true;
+              }
+        else {
+        correctGuess = false;
             }
+        if (correctGuess === true) {
+                answerArray[j] = x;
+                console.log(answerArray);
+                answerArrayDiv.textContent = answerArray;
+            } 
+            else {
+                incorrectChoices.push(x);
+                incorrectChoicesText.textContent = incorrectChoices;
         }
+        
+    }
+        
     }
 
 document.onkeyup = function (event) {
@@ -78,6 +92,7 @@ document.onkeyup = function (event) {
 }
 
 //check out innerHTML versus textContent
+
 
 
 
